fc1924dfff470fa383d0b2098f807c102b7fd6da
Rael <rael.gc@gmail.com>
2014-11-17 18:48:01 -0200

    Moving rvm folder to /usr/share/rvm

--- rvm_add_sudoers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-users=`awk -F':' '/^sudo/{print $4}' /etc/group`
-IFS=',' read -a array <<< "$users"
-for u in "${array[@]}"
-do
-    usermod -a -G rvm $u
-    su - $u -c 'rvm user gemsets'
-done
